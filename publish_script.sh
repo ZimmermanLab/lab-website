d93887110c00af901587cad4db5c66c12dfa50fa
Naupaka Zimmerman <naupaka@gmail.com>
2023-06-07 20:59:59 +0000

    Add verbose flag when removing files when publishing

--- publish_script.sh
+++ publish_script.sh
@@ -6,6 +6,6 @@ rm -rfv /var/www/html/*
 # add the new
 cp -rv docs/* /var/www/html/
 cp -v .well-known/keybase.txt /var/www/html
-rm -rf /var/www/html/.git
-rm /var/www/html/.DS_Store
+rm -rfv /var/www/html/.git
+rm -v /var/www/html/.DS_Store
 
